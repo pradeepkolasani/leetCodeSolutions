db24c17d530fdd099a2b5c054192161e8c41f103
pradeepkolasani <kolasani.chandra@gmail.com>
2020-08-21 12:35:05 +0530

    find duplicate values

---
+++ FindDuplicate.java
@@ -0,0 +1,44 @@
+/*
+ * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+ * */
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int[] a = {1,3,4,2,2};
+        System.out.println(findDuplicate(a));
+    }
+
+    public static int findDuplicate(int[] nums) {
+        List<Integer> requiredList = new ArrayList<>();
+        List<Integer> out =  new ArrayList<>();
+        for(int index = 0; index < nums.length; index++){
+            requiredList.add(nums[index]);
+        }
+        int req = 0;
+        for(int index1 = 0 ; index1 < nums.length; index1++){
+            if(!out.contains(requiredList.get(index1)))
+                out.add(requiredList.get(index1));
+            else {
+                req = requiredList.get(index1);
+                break;
+            }
+        }
+        return req;
+    }
+
+}
